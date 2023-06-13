fc0e9c8b9ca30d3a02b54071ff20debdac9dd24b
Allen Russell <garyrusselljr@gmail.com>
2023-06-13 09:19:02 -0600

    feat: Add preferences and add_preference method

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,19 +1,24 @@
 class Visitor
   attr_reader :name,
               :height,
-              :spending_money
+              :spending_money,
+              :preferences
 
   def initialize(name, height, spending_money)
     @name = name
     @height = height
     @spending_money = remove_dollar_sign(spending_money)
+    @preferences = []
   end
+
+  def add_preference(preference)
+    @preferences << preference
+  end
+
 
   private
 
   def remove_dollar_sign(value)
     value.delete('$').to_i
   end
-
-  public
 end
