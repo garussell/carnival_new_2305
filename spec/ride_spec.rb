5119ab29b9b6ed5cbc3469188050f2fc2c3c0bee
Allen Russell <garyrusselljr@gmail.com>
2023-06-13 09:31:02 -0600

    test: Add setup and test for initialize

---
+++ spec/ride_spec.rb
@@ -0,0 +1,39 @@
+require './spec/spec_helper'
+
+RSpec.describe Ride do
+  before(:each) do
+    # rides
+    @ride1 = Ride.new({ 
+      name: 'Carousel', 
+      min_height: 24, 
+      admission_fee: 1, 
+      excitement: :gentle 
+    })
+    @ride2 = Ride.new({ 
+      name: 'Ferris Wheel', 
+      min_height: 36, 
+      admission_fee: 5, 
+      excitement: :gentle 
+    })
+    @ride3 = Ride.new({ 
+      name: 'Roller Coaster', 
+      min_height: 54, 
+      admission_fee: 2, 
+      excitement: :thrilling 
+    })
+    # visitors
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
+  end
+
+  describe "#initialize" do
+    it "exists and has attributes" do
+      expect(@ride1).to be_an_instance_of(Ride)
+      expect(@ride1.name).to eq("Carousel")
+      expect(@ride1.min_height).to eq(24)
+      expect(@ride1.admission_fee).to eq(1)
+      expect(@ride1.excitement).to eq(:gentle)
+    end
+  end
+end
