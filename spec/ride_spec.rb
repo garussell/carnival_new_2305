d23d25eab0b01f394ea97e6ef00a0e22555111e6
Allen Russell <garyrusselljr@gmail.com>
2023-06-13 10:26:54 -0600

    test: Add test for board_rider

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -38,5 +38,38 @@ RSpec.describe Ride do
     end
   end
 
+  describe "#board_rider(visitor)" do
+    it "can board riders" do
+      @visitor1.add_preference(:gentle)
+      @visitor2.add_preference(:gentle)
 
+      @ride1.board_rider(@visitor1)
+      @ride1.board_rider(@visitor2)
+      @ride1.board_rider(@visitor1)
+
+      expect(@ride1.rider_log).to eq({
+        @visitor1 => 2, 
+        @visitor2 => 1 
+      })
+
+      expect(@visitor1.spending_money).to eq(8)
+      expect(@ride1.total_revenue).to eq(3)
+
+      @visitor2.add_preference(:thrilling)
+      @visitor3.add_preference(:thrilling)
+
+      @ride3.board_rider(@visitor1)
+      @ride3.board_rider(@visitor2)
+      @ride3.board_rider(@visitor3)
+
+      expect(@visitor1.spending_money).to eq(8)
+      expect(@visitor2.spending_money).to eq(4)
+      expect(@visitor3.spending_money).to eq(13)
+
+      expect(@ride3.rider_log).to eq({
+        @visitor3 => 1
+      })
+      expect(@ride3.total_revenue).to eq(2)
+    end
+  end
 end
